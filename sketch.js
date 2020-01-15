a93162815d3a379175325fa5544e66ab87ff4e64
lorenzobarilla <37076142+lorenzobarilla@users.noreply.github.com>
2020-01-15 22:06:29 +0100

    improvements for re-evaluation

--- sketch.js
+++ sketch.js
@@ -1,34 +1,37 @@
+// global variables
 var mic;
 var volume;
 
 var playerX;
 var playerY;
+// array of colors for the player
+var coloreArray = ["red", "yellow", "green", "pink", "blue", "orange", "white"];
+// index for coloreArray
+var colore = 0;
 
+// game status global variables
 var startGame = false;
 var gameover = false;
 var youWin = false;
 
+// barriers global variables
 var allMyBarriers = [];
 var amountOfBarriers = 8;
 
-
-function preload() {
-  // put preload code here
-}
+function preload() {}
 
 function setup() {
-  // put setup code here
   createCanvas(windowWidth, windowHeight);
 
-  //imposta input sul microfono e comincia a rilevare input
+  //set microphone input
   mic = new p5.AudioIn();
   mic.start();
 
-  //player position X & Y
+  //player starting position X & Y
   playerY = windowHeight * 0.4;
   playerX = 25;
 
-  //barriers
+  //barriers creation
   for (var i = 0; i < amountOfBarriers; i++) {
     var tempx = windowWidth / 10 + i * windowWidth / 10;
     var tempy = 0;
@@ -36,28 +39,25 @@ function setup() {
     var tempBarrier = new CreateBarrier(tempx, tempy);
     allMyBarriers.push(tempBarrier);
   }
+  //info gameplay
+  text1 = createDiv("CLICK to start/pause <br> MAKE NOISE to play <br> Press SPACEBAR to change color");
+  text1.style("user-select: none; position: absolute; top: 85%; left: 10%; color: white; text-align: left; font-family: 'Source Code Pro'; font-size: 1vw");
+  text1.hide();
 }
 
 function draw() {
-
   volume = mic.getLevel();
-  clear();
 
   background(0);
 
-  //info gameplay
+  // instruction text visible / invisible
   if (startGame == false) {
-    push();
-    textSize(24);
-    noStroke();
-    fill(255);
-    text("Click & Make Noise", windowWidth / 9, windowHeight * 0.75);
-    pop();
+    text1.show();
+  }
+  if (startGame == true || gameover == true || youWin == true) {
+    text1.hide();
   }
 
-
-  var finishLine = new CreateFinishLine();
-  finishLine.display();
 
   //player
   player = new CreatePlayer(playerX, playerY, windowHeight / 18);
@@ -73,49 +73,52 @@ function draw() {
   //player: gravity & x movement
   if (startGame == true) {
     playerX += 1;
-
+    // gravity makes player fall
     playerY += 3;
+    // sound makes player rise
     playerY -= 150 * volume;
   }
 
+  //Screen Game Over
   if (gameover == true) {
     push();
     background(200, 0, 0);
     textSize(30);
     noStroke();
     fill(255);
+    textAlign(CENTER);
+    textFont('Source Code Pro');
     text("Game Over", windowWidth / 2, windowHeight / 2);
     pop();
+    noLoop();
   }
 
-  //finish
-  if (player.x >= finishLine.x) {
-    youWin = true;
-  }
-
-
+  //Finish line
   if (player.x >= windowWidth * 0.95) {
     youWin = true;
   }
 
-
+  //Screen You Win
   if (youWin == true) {
     push();
     background(0, 200, 0);
     textSize(30);
     noStroke();
     fill(255);
+    textAlign(CENTER);
+    textFont('Source Code Pro');
     text("You Win", windowWidth / 2, windowHeight / 2);
     pop();
   }
 }
 
+// player object
 function CreatePlayer(_x, _y, _r) {
   this.x = _x;
   this.y = _y;
   this.r = _r;
 
-  this.color = "red";
+  this.color = coloreArray[colore];
   this.strokeWeight = 1;
 
   this.display = function() {
@@ -125,11 +128,12 @@ function CreatePlayer(_x, _y, _r) {
   }
 }
 
+// barriers object
 function CreateBarrier(_x, _y) {
   this.x = _x;
-  this.y = windowHeight*0.7;
-  this.y1 = _y + 100;
-  this.y2 = _y - 100 - windowHeight;
+  this.y = windowHeight * 0.7;
+  this.y1 = _y + 125;
+  this.y2 = _y - 125 - windowHeight;
 
   this.color = 125;
 
@@ -147,32 +151,10 @@ function CreateBarrier(_x, _y) {
     pop();
   }
 
-//clean
-  this.collide2 = function() {
-    //if (this.x > player.x + player.width / 2 && this.x+this.width < player.x - player.width/2) {
-    //if(this.x < player.x + player.width/2  && this.x+this.width > player.x - player.width/2) {
-    if (player.x + player.r / 2 < this.x || player.x - player.r / 2 > this.x + this.width) {} else {
-      if (player.y + player.r / 2 < this.y || player.y - player.r / 2 > this.y + this.height) {} else {
-        console.log("gameover");
-        gameover = true;
-      }
-    }
-  }
-
-  this.collide3 = function() {
-    if ((player.x + player.r / 2 > this.x) && (player.x - player.r / 2 < this.x + this.width)) {
-      if ((player.y + player.r / 2 > this.y) && (player.y - player.r / 2 < this.y + this.height)) {
-        console.log("gameover");
-        gameover = true;
-      }
-    }
-  }
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // set collision between player and barriers
   this.collide = function() {
-    //if ((player.x + player.r / 2 > this.x) && (player.x - player.r / 2 < this.x + this.width) && (player.y + player.r / 2 > this.y) && (player.y - player.r / 2 < this.y + this.height)) {
-      if(player.x + player.r / 2 > this.x && player.x - player.r / 2 < this.x + this.width) {
-        if(player.y > windowHeight/2 + 100 || player.y < windowHeight/2 - 100) {
+    if (player.x + player.r / 2 > this.x && player.x - player.r / 2 < this.x + this.width) {
+      if (player.y > windowHeight / 2 + 125 || player.y < windowHeight / 2 - 125) {
         console.log("gameover");
         gameover = true;
       }
@@ -180,28 +162,36 @@ function CreateBarrier(_x, _y) {
   }
 }
 
-//clean
-function CreateFinishLine() {
-  this.display = function() {
-    push();
-    fill(255);
-    rect(15, windowHeight, windowWidth * 0.9, 0);
-    pop();
-  }
-}
-/////////////////////////////////////////////////////
+
 function mouseClicked() {
   if (gameover == false) {
+    // mouseclick play-pause the game if gameover is false
     if (startGame == false) {
       startGame = true;
     } else {
       startGame = false;
     }
   } else {
+    // refresh the page if gameover is true
     location = location;
   }
 
+  // refresh the page if the player has won
   if (youWin == true) {
     location = location;
   }
+}
+
+// keyboard input
+function keyPressed() {
+  if (keyCode === 32) {
+    // SPACEBAR generates a random number to change index in coloreArray and change player color
+    colore = round(random(0, 6));
+    return false;
+  }
+}
+
+//resize
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
